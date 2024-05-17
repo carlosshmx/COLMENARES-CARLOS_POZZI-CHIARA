0216a749f0e7712ead46eefa0ecd6c76c0157500
Carlos Colmenares <carlosshmx@hotmail.com>
2024-05-16 21:08:16 -0300

    reacomodado de codigo

--- src/com/backend/Main.java
+++ src/com/backend/Main.java
@@ -5,70 +5,14 @@ import org.apache.log4j.Logger;
 
 import java.sql.*;
 
-import static com.backend.repository.dbconnection.H2Connection.getConnection;
+import static com.backend.repository.dbconnection.H2Connection.crearTablas;
 
 
 public class Main {
 
     private static final Logger LOGGER = Logger.getLogger(Main.class);
     public static void main(String[] args) {
-
-        String create = "DROP TABLE IF EXISTS ANIMALES; CREATE TABLE ANIMALES(ID INT AUTO_INCREMENT PRIMARY KEY, NOMBRE VARCHAR(20) NOT NULL, TIPO VARCHAR(20) NOT NULL)";
-        String insert = "INSERT INTO ANIMALES(NOMBRE, TIPO) VALUES ('Firulais', 'Perro'), ('Lola', 'Vaca'), ('Homero', 'Perro'), ('Pepe', 'Sapo'), ('Tuki', 'Loro')";
-
-        String select = "SELECT * FROM ANIMALES";
-
-        Connection connection = null;
-
-        try{
-            connection = getConnection();
-
-            //crear la tabla
-            Statement statement = connection.createStatement();
-            statement.execute(create);
-
-            //insertar los registros
-            statement.execute(insert);
-
-            //select all
-            ResultSet resultSet = statement.executeQuery(select);
-
-            //recorrer el result set
-            while (resultSet.next()){
-                LOGGER.info("Animal: " + resultSet.getString("id") + " - " + resultSet.getString(2) + " - " + resultSet.getString("tipo"));
-            }
-
-            LOGGER.info("-----------------------------------------------------");
-            //eliminar un registro
-            statement.execute("DELETE FROM ANIMALES WHERE ID = 1");
-
-            //select all
-            resultSet = statement.executeQuery(select);
-            //recorrer el result set
-            while (resultSet.next()){
-                LOGGER.info("Animal: " + resultSet.getString("id") + " - " + resultSet.getString(2) + " - " + resultSet.getString("tipo"));
-            }
-
-
-
-        }catch (Exception exception){
-            exception.printStackTrace();
-            LOGGER.error(exception.getClass() + ": " + exception.getMessage());
-        }
-
-        finally {
-            try{
-                connection.close();
-            } catch (Exception exception){
-                LOGGER.error(exception.getMessage());
-            }
-        }
+        crearTablas();
 
     }
-    /*public static Connection getConnection() throws ClassNotFoundException, SQLException {
-        //indicar que driver vamos a usar
-        Class.forName("org.h2.Driver");
-        return DriverManager.getConnection("jdbc:h2:~/clase8c1b2", "sa", "sa");
-
-    }*/
 }
--- src/com/backend/repository/IDao.java
+++ src/com/backend/repository/IDao.java
@@ -1,7 +1,9 @@
 package com.backend.repository;
 
+import java.util.List;
+
 public interface IDao <T>{
     T registrar(T var1);
-
     T buscarPorId(Long var1);
+    List<T> listarTodos();
 }
